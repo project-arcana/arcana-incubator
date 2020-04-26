59610b84aec3e8e66578a88856468d55f9a1468f
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-04-26 14:50:12 +0200

    Fix freefly cam lefthandedness

--- src/arcana-incubator/device-abstraction/freefly_camera.hh
+++ src/arcana-incubator/device-abstraction/freefly_camera.hh
@@ -10,30 +10,12 @@ typedef struct SDL_Window SDL_Window;
 
 namespace inc::da
 {
-/// Smoothed lerp alpha, framerate-correct
-inline float smooth_lerp_alpha(float smoothing, float dt) { return 1 - std::pow(smoothing, dt); }
-/// Exponential decay alpha, framerate-correct damp / lerp
-inline float exponential_decay_alpha(float lambda, float dt) { return 1 - std::exp(-lambda * dt); }
-/// alpha based on the halftime between current and target state
-inline float halftime_lerp_alpha(float halftime, float dt) { return 1 - std::pow(.5f, dt / halftime); }
+struct input_manager;
 
-constexpr float halton_sequence(int index, int base)
-{
-    float f = 1.f;
-    float r = 0.f;
-    while (index > 0)
-    {
-        f = f / float(base);
-        r = r + f * float(index % base);
-        index = index / base;
-    }
-    return r;
-}
-
-
+/// simple camera state, assumes LHS world space
 struct fps_cam_state
 {
-    tg::pos3 position = tg::pos3(0, 0, 5);
+    tg::pos3 position = tg::pos3(0);
     tg::vec3 forward = tg::vec3(0, 0, 1);
 
     void move_relative(tg::vec3 distance);
@@ -41,8 +23,8 @@ struct fps_cam_state
     static tg::quat forward_to_rotation(tg::vec3 fwd, tg::vec3 up = {0, 1, 0});
 };
 
-struct input_manager;
-
+/// exponential smoothing camera, assumes LHS world space
+/// has provisions for out of the box input handling
 struct smooth_fps_cam
 {
     fps_cam_state physical;
@@ -65,4 +47,23 @@ private:
     bool _mouse_captured = false;
 };
 
+/// Smoothed lerp alpha, framerate-correct
+inline float smooth_lerp_alpha(float smoothing, float dt) { return 1 - std::pow(smoothing, dt); }
+/// Exponential decay alpha, framerate-correct damp / lerp
+inline float exponential_decay_alpha(float lambda, float dt) { return 1 - std::exp(-lambda * dt); }
+/// alpha based on the halftime between current and target state
+inline float halftime_lerp_alpha(float halftime, float dt) { return 1 - std::pow(.5f, dt / halftime); }
+
+constexpr float halton_sequence(int index, int base)
+{
+    float f = 1.f;
+    float r = 0.f;
+    while (index > 0)
+    {
+        f = f / float(base);
+        r = r + f * float(index % base);
+        index = index / base;
+    }
+    return r;
+}
 }
