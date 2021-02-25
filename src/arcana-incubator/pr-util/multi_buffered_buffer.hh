86e720698b88d6c209d5660178845e51d56d612b
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2021-02-25 08:40:19 +0100

    Change multi_buffered_buffer api

--- src/arcana-incubator/pr-util/multi_buffered_buffer.hh
+++ src/arcana-incubator/pr-util/multi_buffered_buffer.hh
@@ -10,17 +10,23 @@ namespace inc::pre
 /// (easier way to avoid this issue: use cached buffers)
 struct multi_buffered_buffer
 {
-    void initialize(pr::Context& ctx, unsigned num_backbuffers, pr::buffer_info const& info);
-    void initialize(pr::Context& ctx, pr::swapchain sc, pr::buffer_info const& info);
+    void initialize(pr::Context& ctx, phi::resource_heap heap, uint32_t size_bytes, uint32_t stride_bytes, char const* debug_name = nullptr, uint32_t num_backbuffers = 3u);
+
+    void initialize_readback(pr::Context& ctx, uint32_t size_bytes, uint32_t stride_bytes, char const* debug_name = nullptr, uint32_t num_backbuffers = 3u)
+    {
+        initialize(ctx, phi::resource_heap::readback, size_bytes, stride_bytes, debug_name, num_backbuffers);
+    }
+
+    void initialize_upload(pr::Context& ctx, uint32_t size_bytes, uint32_t stride_bytes, char const* debug_name = nullptr, uint32_t num_backbuffers = 3u)
+    {
+        initialize(ctx, phi::resource_heap::upload, size_bytes, stride_bytes, debug_name, num_backbuffers);
+    }
+
+    [[deprecated]] void initialize(pr::Context& ctx, unsigned num_backbuffers, pr::buffer_info const& info);
 
     void destroy(pr::Context& ctx);
 
     multi_buffered_buffer() = default;
-    multi_buffered_buffer(pr::Context& ctx, pr::swapchain sc, unsigned size, unsigned stride = 0, pr::resource_heap heap = pr::resource_heap::upload, bool allow_uav = false)
-    {
-        initialize(ctx, sc, pr::buffer_info::create(size, stride, heap, allow_uav));
-    }
-
     multi_buffered_buffer(multi_buffered_buffer const&) = delete;
     multi_buffered_buffer(multi_buffered_buffer&& rhs) noexcept = default;
     multi_buffered_buffer& operator=(multi_buffered_buffer&& rhs) noexcept = default;
